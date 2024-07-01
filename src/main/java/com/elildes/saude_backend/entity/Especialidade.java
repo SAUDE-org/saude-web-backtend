9271d584dd8c53f4f3d21ed2cb15d08b66bcca06
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-01 02:09:30 -0300

    feat-enum: adiciona enumeração 'Especialidade'

---
+++ src/main/java/com/elildes/saude_backend/entity/Especialidade.java
@@ -0,0 +1,11 @@
+package com.elildes.saude_backend.entity;
+
+public enum Especialidade {
+    ENFERMAGEM,
+    ENDOCRINOLOGIA,
+    FISIOTERAPIA,
+    HOME_CARE,
+    NUTRICIONISTA,
+    ODONTOLOGIA,
+    PSICOLOGIA
+}
