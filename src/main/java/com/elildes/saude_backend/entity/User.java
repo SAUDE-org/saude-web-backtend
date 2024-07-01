9a9c09a4e2ceb515aab00c17949732260c87b4b7
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-30 21:07:01 -0300

    fix-entiey: rafatora entidade 'User' e adiciona métodos

--- src/main/java/com/elildes/saude_backend/entity/User.java
+++ src/main/java/com/elildes/saude_backend/entity/User.java
@@ -24,4 +24,96 @@ public class User {
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    public User(){};
+
+    public User(Long id_user, String name, String username, String password) {
+        this.id_user = id_user;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(Long id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id_user == null) ? 0 : id_user.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id_user == null) {
+            if (other.id_user != null)
+                return false;
+        } else if (!id_user.equals(other.id_user))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id_user=" + id_user + ", name=" + name + ", username=" + username + ", password=" + password
+                + ", getId_user()=" + getId_user() + ", getName()=" + getName() + ", getUsername()=" + getUsername()
+                + ", getPassword()=" + getPassword() + "]";
+    }   
+    
 }
