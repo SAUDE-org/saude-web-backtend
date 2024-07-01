67c5aa519cc2908a4079b578c0910d463612499d
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-30 23:04:01 -0300

    feat-entity: mapeamento JPA da entidade 'Profissional'

--- src/main/java/com/elildes/saude_backend/entity/Profissional.java
+++ src/main/java/com/elildes/saude_backend/entity/Profissional.java
@@ -1,17 +1,45 @@
 package com.elildes.saude_backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "profissional")
 public class Profissional extends Pessoa {
     
+    @Id
+    @GeneratedValue
+    @Column(name = "id_profissional", nullable = false, unique = true)
+    private String id_profissional;
+
+    @Column(name = "registro_pro", nullable = false, unique = true)
     private String registro_pro;
 
     public Profissional(){};
 
-    public Profissional(String registro_pro) {
+    public Profissional(String id_profissional, String registro_pro) {
+        this.id_profissional = id_profissional;
+        this.registro_pro = registro_pro;
+    }
+
+    public Profissional(Long id_pessoa, String nome, String cpf, String email, String telefone, String endereco,
+            String id_profissional, String registro_pro) {
+        super(id_pessoa, nome, cpf, email, telefone, endereco);
+        this.id_profissional = id_profissional;
         this.registro_pro = registro_pro;
     }
 
     public String getRegistro_pro() {
         return registro_pro;
+    }
+
+    public String getId_profissional() {
+        return id_profissional;
+    }
+
+    public void setId_profissional(String id_profissional) {
+        this.id_profissional = id_profissional;
     }
 
     public void setRegistro_pro(String registro_pro) {
@@ -22,6 +50,7 @@ public class Profissional extends Pessoa {
     public int hashCode() {
         final int prime = 31;
         int result = super.hashCode();
+        result = prime * result + ((id_profissional == null) ? 0 : id_profissional.hashCode());
         result = prime * result + ((registro_pro == null) ? 0 : registro_pro.hashCode());
         return result;
     }
@@ -35,6 +64,11 @@ public class Profissional extends Pessoa {
         if (getClass() != obj.getClass())
             return false;
         Profissional other = (Profissional) obj;
+        if (id_profissional == null) {
+            if (other.id_profissional != null)
+                return false;
+        } else if (!id_profissional.equals(other.id_profissional))
+            return false;
         if (registro_pro == null) {
             if (other.registro_pro != null)
                 return false;
@@ -45,11 +79,12 @@ public class Profissional extends Pessoa {
 
     @Override
     public String toString() {
-        return "Profissional [registro_pro=" + registro_pro + ", getRegistro_pro()=" + getRegistro_pro()
-                + ", getId_pessoa()=" + getId_pessoa() + ", getNome()=" + getNome() + ", hashCode()=" + hashCode()
-                + ", getCpf()=" + getCpf() + ", getEmail()=" + getEmail() + ", getTelefone()=" + getTelefone()
-                + ", getEndereco()=" + getEndereco() + ", getClass()=" + getClass() + ", toString()=" + super.toString()
-                + "]";
+        return "Profissional [id_profissional=" + id_profissional + ", registro_pro=" + registro_pro
+                + ", getId_pessoa()=" + getId_pessoa() + ", getRegistro_pro()=" + getRegistro_pro()
+                + ", getId_profissional()=" + getId_profissional() + ", getNome()=" + getNome() + ", getCpf()="
+                + getCpf() + ", hashCode()=" + hashCode() + ", getEmail()=" + getEmail() + ", getClass()=" + getClass()
+                + ", getTelefone()=" + getTelefone() + ", getEndereco()=" + getEndereco() + ", toString()="
+                + super.toString() + "]";
     }
 
 }
