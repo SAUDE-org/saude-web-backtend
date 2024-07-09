a8d668df46364f62d32db04dfd0a5cad9d453780
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-09 00:11:48 -0300

    fix-entity: ajusta imports pata versão Spring Boot 2.7.18

--- src/main/java/com/elildes/saude_backend/models/Paciente.java
+++ src/main/java/com/elildes/saude_backend/models/Paciente.java
@@ -1,68 +1,155 @@
 package com.elildes.saude_backend.models;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.SequenceGenerator;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
-// @Entity(name = "paciente")
-public class Paciente extends Pessoa {
+@Entity(name = "paciente")
+public class Paciente {
     
-    // @Id
-    // @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="paciente_seq")
-	// @SequenceGenerator(name="paciente_seq", sequenceName="paciente_seq", allocationSize=1)
-    // @Column(name = "id_paciente", nullable = false, unique = true)
-    // private Long id_paciente;
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="paciente_seq")
+	@SequenceGenerator(name="paciente_seq", sequenceName="paciente_seq", allocationSize=1)
+    @Column(name = "id_paciente", nullable = false, unique = true)
+    private Long id_paciente;
 
-    // public Paciente(){};
+    @Column(name = "nome", nullable = false)
+    private String nome;
 
-    // public Paciente(Long id_pessoa, String nome, String cpf, String email, String telefone, String endereco,
-    //         Long id_paciente) {
-    //     super(id_pessoa, nome, cpf, email, telefone, endereco);
-    //     this.id_paciente = id_paciente;
-    // }
+    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    private String cpf;
 
-    // public Long getId_paciente() {
-    //     return id_paciente;
-    // }
+    @Column(name = "email")
+    private String email;
 
-    // public void setId_paciente(Long id_paciente) {
-    //     this.id_paciente = id_paciente;
-    // }    
+    @Column(name = "telefone")
+    private String telefone;
 
-    // @Override
-    // public int hashCode() {
-    //     final int prime = 31;
-    //     int result = super.hashCode();
-    //     result = prime * result + ((id_paciente == null) ? 0 : id_paciente.hashCode());
-    //     return result;
-    // }
+    @Column(name = "endereco")
+    private String endereco;
 
-    // @Override
-    // public boolean equals(Object obj) {
-    //     if (this == obj)
-    //         return true;
-    //     if (!super.equals(obj))
-    //         return false;
-    //     if (getClass() != obj.getClass())
-    //         return false;
-    //     Paciente other = (Paciente) obj;
-    //     if (id_paciente == null) {
-    //         if (other.id_paciente != null)
-    //             return false;
-    //     } else if (!id_paciente.equals(other.id_paciente))
-    //         return false;
-    //     return true;
-    // }
+    public Paciente(){};
 
-    // @Override
-    // public String toString() {
-    //     return "Paciente [id_paciente=" + id_paciente + ", getId_paciente()=" + getId_paciente() + ", hashCode()="
-    //             + hashCode() + ", getId_pessoa()=" + getId_pessoa() + ", getNome()=" + getNome() + ", getClass()="
-    //             + getClass() + ", getCpf()=" + getCpf() + ", getEmail()=" + getEmail() + ", getTelefone()="
-    //             + getTelefone() + ", getEndereco()=" + getEndereco() + ", toString()=" + super.toString() + "]";
-    // }    
+    public Paciente(Long id_paciente, String nome, String cpf, String email, String telefone, String endereco) {
+        this.id_paciente = id_paciente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
 
+    public Long getId_paciente() {
+        return this.id_paciente;
+    }
+
+    public void setId_paciente(Long id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id_paciente == null) ? 0 : id_paciente.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Paciente other = (Paciente) obj;
+        if (id_paciente == null) {
+            if (other.id_paciente != null)
+                return false;
+        } else if (!id_paciente.equals(other.id_paciente))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        if (endereco == null) {
+            if (other.endereco != null)
+                return false;
+        } else if (!endereco.equals(other.endereco))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente [id_paciente=" + id_paciente + ", nome=" + nome + ", cpf=" + cpf + ", email=" + email
+                + ", telefone=" + telefone + ", endereco=" + endereco + "]";
+    }  
+    
+    
 }
--- src/main/java/com/elildes/saude_backend/models/Profissional.java
+++ src/main/java/com/elildes/saude_backend/models/Profissional.java
@@ -1,97 +1,196 @@
 package com.elildes.saude_backend.models;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.EnumType;
-// import jakarta.persistence.Enumerated;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
-// @Entity(name = "profissional")
+import com.elildes.saude_backend.models.enums.Especialidade;
+
+@Entity(name = "profissional")
 public class Profissional extends Pessoa {
     
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.AUTO)
-    // @Column(name = "id_profissional", nullable = false, unique = true)
-    // private Long id_profissional;
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="pro_seq")
+    @SequenceGenerator(name="pro_seq", sequenceName="pro_seq", allocationSize=1)
+    @Column(name = "id_profissional", nullable = false, unique = true)
+    private Long id_profissional;
 
-    // @Column(name = "registro_pro", nullable = false, unique = true)
-    // private String registro_pro;
+    @Column(name = "nome", nullable = false)
+    private String nome;
 
-    // @Enumerated(EnumType.STRING)
-    // @Column(name = "especialidade")
-    // private Especialidade especialidade;
+    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    private String cpf;
 
-    // public Profissional(){};
+    @Column(name = "email")
+    private String email;
 
-    // public Profissional(String id_profissional, String registro_pro) {
-    //     this.id_profissional = id_profissional;
-    //     this.registro_pro = registro_pro;
-    // }
+    @Column(name = "telefone")
+    private String telefone;
 
-    // public Profissional(Long id_pessoa, String nome, String cpf, String email, String telefone, String endereco,
-    //         String id_profissional, String registro_pro) {
-    //     super(id_pessoa, nome, cpf, email, telefone, endereco);
-    //     this.id_profissional = id_profissional;
-    //     this.registro_pro = registro_pro;
-    // }
+    @Column(name = "endereco")
+    private String endereco;
 
-    // public String getRegistro_pro() {
-    //     return registro_pro;
-    // }
+    @Column(name = "registro_pro", nullable = false, unique = true)
+    private String registro_pro;
 
-    // public String getId_profissional() {
-    //     return id_profissional;
-    // }
+    @Enumerated(EnumType.STRING)
+    @Column(name = "especialidade")
+    private Especialidade especialidade;
 
-    // public void setId_profissional(String id_profissional) {
-    //     this.id_profissional = id_profissional;
-    // }
+    public Profissional(){}    
 
-    // public void setRegistro_pro(String registro_pro) {
-    //     this.registro_pro = registro_pro;
-    // }
+    public Profissional(Long id_profissional, String nome, String cpf, String email, String telefone, String endereco,
+            String registro_pro, Especialidade especialidade) {
+        this.id_profissional = id_profissional;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.registro_pro = registro_pro;
+        this.especialidade = especialidade;
+    }
 
-    // @Override
-    // public int hashCode() {
-    //     final int prime = 31;
-    //     int result = super.hashCode();
-    //     result = prime * result + ((id_profissional == null) ? 0 : id_profissional.hashCode());
-    //     result = prime * result + ((registro_pro == null) ? 0 : registro_pro.hashCode());
-    //     return result;
-    // }
+    public Long getId_profissional() {
+        return id_profissional;
+    }
 
-    // @Override
-    // public boolean equals(Object obj) {
-    //     if (this == obj)
-    //         return true;
-    //     if (!super.equals(obj))
-    //         return false;
-    //     if (getClass() != obj.getClass())
-    //         return false;
-    //     Profissional other = (Profissional) obj;
-    //     if (id_profissional == null) {
-    //         if (other.id_profissional != null)
-    //             return false;
-    //     } else if (!id_profissional.equals(other.id_profissional))
-    //         return false;
-    //     if (registro_pro == null) {
-    //         if (other.registro_pro != null)
-    //             return false;
-    //     } else if (!registro_pro.equals(other.registro_pro))
-    //         return false;
-    //     return true;
-    // }
+    public void setId_profissional(Long id_profissional) {
+        this.id_profissional = id_profissional;
+    }
 
-    // @Override
-    // public String toString() {
-    //     return "Profissional [id_profissional=" + id_profissional + ", registro_pro=" + registro_pro
-    //             + ", getId_pessoa()=" + getId_pessoa() + ", getRegistro_pro()=" + getRegistro_pro()
-    //             + ", getId_profissional()=" + getId_profissional() + ", getNome()=" + getNome() + ", getCpf()="
-    //             + getCpf() + ", hashCode()=" + hashCode() + ", getEmail()=" + getEmail() + ", getClass()=" + getClass()
-    //             + ", getTelefone()=" + getTelefone() + ", getEndereco()=" + getEndereco() + ", toString()="
-    //             + super.toString() + "]";
-    // }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getRegistro_pro() {
+        return registro_pro;
+    }
+
+    public void setRegistro_pro(String registro_pro) {
+        this.registro_pro = registro_pro;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id_profissional == null) ? 0 : id_profissional.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+        result = prime * result + ((registro_pro == null) ? 0 : registro_pro.hashCode());
+        result = prime * result + ((especialidade == null) ? 0 : especialidade.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Profissional other = (Profissional) obj;
+        if (id_profissional == null) {
+            if (other.id_profissional != null)
+                return false;
+        } else if (!id_profissional.equals(other.id_profissional))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        if (endereco == null) {
+            if (other.endereco != null)
+                return false;
+        } else if (!endereco.equals(other.endereco))
+            return false;
+        if (registro_pro == null) {
+            if (other.registro_pro != null)
+                return false;
+        } else if (!registro_pro.equals(other.registro_pro))
+            return false;
+        if (especialidade != other.especialidade)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Profissional [id_profissional=" + id_profissional + ", nome=" + nome + ", cpf=" + cpf + ", email="
+                + email + ", telefone=" + telefone + ", endereco=" + endereco + ", registro_pro=" + registro_pro
+                + ", especialidade=" + especialidade + "]";
+    };
+
+    
 
 }
