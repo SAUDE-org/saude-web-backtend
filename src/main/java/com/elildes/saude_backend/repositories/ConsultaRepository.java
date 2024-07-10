59bb55403559d18849ceaaf24eb3e3ad2fd092b5
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-10 17:36:54 -0300

    fix: adiciona anotação em repository

--- src/main/java/com/elildes/saude_backend/repositories/ConsultaRepository.java
+++ src/main/java/com/elildes/saude_backend/repositories/ConsultaRepository.java
@@ -1,9 +1,11 @@
 package com.elildes.saude_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.elildes.saude_backend.models.Consulta;
 
+@Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
 }
