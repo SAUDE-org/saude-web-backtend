89421582dd48119476ffcabd4f7994824f9bf048
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-10 19:23:16 -0300

    feat: implementa em ConsultaController o método buscar a consulta com a data mais próxima

---
+++ src/main/java/com/elildes/saude_backend/controllers/ConsultaController.java
@@ -0,0 +1,33 @@
+package com.elildes.saude_backend.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elildes.saude_backend.models.Consulta;
+import com.elildes.saude_backend.services.ConsultaService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/consulta")
+public class ConsultaController {
+
+    final ConsultaService consultaService;
+
+    public ConsultaController(ConsultaService consultaService) {
+        this.consultaService = consultaService;
+    }
+
+    @GetMapping("/find-next")
+    public ResponseEntity<Consulta> findNextConsulta() {
+        Optional<Consulta> nextConsulta = consultaService.findNextConsulta();
+        if (nextConsulta.isPresent()) {
+            return ResponseEntity.ok(nextConsulta.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+    }
+}
