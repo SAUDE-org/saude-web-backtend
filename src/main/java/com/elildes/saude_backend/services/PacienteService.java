0f01b9888d2613fa02a97143daeaa5d83f6e2586
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-11 22:51:21 -0300

    feat-service: implementa método buscarPacientePorCpf em Pacienteservice

--- src/main/java/com/elildes/saude_backend/services/PacienteService.java
+++ src/main/java/com/elildes/saude_backend/services/PacienteService.java
@@ -1,6 +1,7 @@
 package com.elildes.saude_backend.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,11 @@ public class PacienteService {
     // buscar todos pacientes
     public List<Paciente> buscarTodosPacientes() {
         return pacienteRepository.findAll();
+    }
+
+    // buscar paciente por cpf
+    public Optional<Paciente> buscarPacientePorCpf(String cpf) {
+        return pacienteRepository.findByCpf(cpf);
     }
 
 }
