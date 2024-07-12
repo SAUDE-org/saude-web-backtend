68d025dd849368b518f711290f5a8e43f487157f
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-12 13:39:29 -0300

    feat-controller: implementa método/endpoint getProfissionalPorCpf em ProfissionalController

--- src/main/java/com/elildes/saude_backend/controllers/ProfissionalController.java
+++ src/main/java/com/elildes/saude_backend/controllers/ProfissionalController.java
@@ -1,11 +1,13 @@
 package com.elildes.saude_backend.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +42,13 @@ public class ProfissionalController {
         } else {
             return ResponseEntity.ok(profissionais);
         }
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<Profissional> getProfissionalPorCpf(@PathVariable String cpf) {
+        Optional<Profissional> profissional = profissionalService.buscarProfissionalPorCpf(cpf);
+        return profissional.map(ResponseEntity::ok)
+                           .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
 
