32c6797fc0f88cca2dc67f8862a92fc2c608a217
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-12 17:34:28 -0300

    feat-service: implementa método buscarClinicaPorCnpj em ClinicaService

--- src/main/java/com/elildes/saude_backend/services/ClinicaService.java
+++ src/main/java/com/elildes/saude_backend/services/ClinicaService.java
@@ -1,6 +1,7 @@
 package com.elildes.saude_backend.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,11 @@ public class ClinicaService {
     // buscar todas clínicas
     public List<Clinica> buscarTodasClinicas() {
         return clinicaRepository.findAll();
+    }
+
+    // buscar clínica por cnpj
+    public Optional<Clinica> buscarClinicaPorCnpj(String cnpj) {
+        return clinicaRepository.findByCnpj(cnpj);
     }
 
 
