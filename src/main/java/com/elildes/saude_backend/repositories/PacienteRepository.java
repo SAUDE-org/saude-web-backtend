230ae5300380aa165dd6335690abd13255ecb733
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-11 22:31:09 -0300

    feat-repository: implementa método findByCpf em PacienteRepositpry

--- src/main/java/com/elildes/saude_backend/repositories/PacienteRepository.java
+++ src/main/java/com/elildes/saude_backend/repositories/PacienteRepository.java
@@ -1,5 +1,7 @@
 package com.elildes.saude_backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.elildes.saude_backend.models.Paciente;
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
+    Optional<Paciente> findByCpf(String cpf);
 }
