d70c94dbaff322f3c649d9d1f836494a8ef68361
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-11 23:00:56 -0300

    feat-controller: implementa método getPacientePorCpf em PacienteController

--- src/main/java/com/elildes/saude_backend/controllers/PacienteController.java
+++ src/main/java/com/elildes/saude_backend/controllers/PacienteController.java
@@ -1,11 +1,13 @@
 package com.elildes.saude_backend.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +42,13 @@ public class PacienteController {
         } else {
             return ResponseEntity.ok(pacientes);
         }
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<Paciente> getPacientePorCpf(@PathVariable String cpf) {
+        Optional<Paciente> paciente = pacienteService.buscarPacientePorCpf(cpf);
+        return paciente.map(ResponseEntity::ok)
+                       .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
 }
