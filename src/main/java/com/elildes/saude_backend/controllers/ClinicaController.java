af151a374fd81f3e29e33cce993392ccea461fd4
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-12 17:40:52 -0300

    feat-controller: implementa método/endpoint getClinicaPorCnpj em ClinicaController

--- src/main/java/com/elildes/saude_backend/controllers/ClinicaController.java
+++ src/main/java/com/elildes/saude_backend/controllers/ClinicaController.java
@@ -1,11 +1,13 @@
 package com.elildes.saude_backend.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +42,13 @@ public class ClinicaController {
         } else {
             return ResponseEntity.ok(clinicas);
         }
+    }
+
+    @GetMapping("/cnpj/{cnpj}")
+    public ResponseEntity<Clinica> getClinicaPorCnpj(@PathVariable String cnpj) {
+        Optional<Clinica> clinica = clinicaService.buscarClinicaPorCnpj(cnpj);
+        return clinica.map(ResponseEntity::ok)
+                      .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
 
