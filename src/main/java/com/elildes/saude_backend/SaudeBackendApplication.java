de22db88cdb8a0ce1677c0bb12eeb541ef678108
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-13 13:16:26 -0300

    fix-pom: adiciona dependência jackson-databind

--- src/main/java/com/elildes/saude_backend/SaudeBackendApplication.java
+++ src/main/java/com/elildes/saude_backend/SaudeBackendApplication.java
@@ -1,5 +1,7 @@
 package com.elildes.saude_backend;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SaudeBackendApplication {
 
 	public static void main(String[] args) {
+		// Verifica a configuração regional
+        Locale currentLocale = Locale.getDefault();
+        System.out.println("Localidade configurada: " + currentLocale);
 		SpringApplication.run(SaudeBackendApplication.class, args);
 	}
 
