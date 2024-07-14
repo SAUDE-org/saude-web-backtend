678b5101091798ec74b4058b63669fe0ad010dc4
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-13 23:36:50 -0300

    fix-import: remove imports

--- src/main/java/com/elildes/saude_backend/models/Consulta.java
+++ src/main/java/com/elildes/saude_backend/models/Consulta.java
@@ -5,15 +5,12 @@ import java.time.LocalTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity(name = "consulta")
 public class Consulta {
