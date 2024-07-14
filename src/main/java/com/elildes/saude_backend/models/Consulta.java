136d412d46b1856d4bac22dfc2c8e23c76328a76
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-13 23:35:56 -0300

    fix-entity: altera entidade Consulta

--- src/main/java/com/elildes/saude_backend/models/Consulta.java
+++ src/main/java/com/elildes/saude_backend/models/Consulta.java
@@ -24,22 +24,37 @@ public class Consulta {
     @Column(name = "id_consulta", nullable = false, unique = true)
     private Long id_consulta;
 
+    // @Column(name = "data")
+    // @JsonFormat(pattern = "dd/MM/yyyy")
+    // private LocalDate data;
+
     @Column(name = "data")
-    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate data;
 
     @Column(name = "horario")
     private LocalTime horario;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "paciente_id")
+    // private Paciente paciente;
+
+    @ManyToOne()
     @JoinColumn(name = "paciente_id")
     private Paciente paciente;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "profissional_id")
+    // private Profissional profissional;
+
+    @ManyToOne()
     @JoinColumn(name = "profissional_id")
     private Profissional profissional;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "clinica_id")
+    // private Clinica clinica;
+
+    @ManyToOne()
     @JoinColumn(name = "clinica_id")
     private Clinica clinica;
 
@@ -90,7 +105,6 @@ public class Consulta {
     public void setClinica(Clinica clinica) {
         this.clinica = clinica;
     }
-
     
 
 }
