30bcfc41ac540d0e42c1fd4dc00a7b4d0da25eb7
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-15 20:12:42 -0300

    fix-impport: remove imports

--- src/main/java/com/elildes/saude_backend/configs/SecurityConfig.java
+++ src/main/java/com/elildes/saude_backend/configs/SecurityConfig.java
@@ -2,9 +2,6 @@ package com.elildes.saude_backend.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
