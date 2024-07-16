944045a60e4d16398d97e6bdeb55758535aa938b
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-15 21:21:42 -0300

    feat-config: adiciona webConfig

---
+++ src/main/java/com/elildes/saude_backend/configs/WebConfig.java
@@ -0,0 +1,27 @@
+package com.elildes.saude_backend.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Permitir acesso a API de qualquer lugar
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
